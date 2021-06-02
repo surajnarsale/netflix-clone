30e9f12885a6112f1e963c4c1bb409f684e5516d
Suraj Narsale <surajsnarsale@gmail.com>
2021-06-03 02:24:43 +0530

    Responsive Jumbotron

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -24,15 +24,19 @@ export default function Jumbotron({
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
 	return <Container {...restProps}>{children}</Container>;
 };
+
 Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
 	return <Pane {...restProps}>{children}</Pane>;
 };
+
 Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
 	return <Title {...restProps}>{children}</Title>;
 };
+
 Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
 	return <SubTitle {...restProps}>{children}</SubTitle>;
 };
+
 Jumbotron.Image = function JumbotronImage({ children, ...restProps }) {
 	return <Image {...restProps}>{children}</Image>;
 };
