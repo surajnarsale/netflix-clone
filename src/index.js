c0c967b4cc6fefe9de8c32d4d9b2bf1c7f495900
Suraj Narsale <surajsnarsale@gmail.com>
2021-06-18 08:00:36 +0530

    feat: add context for firebase data

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import App from './App';
 import { GlobalStyles } from './global-styles';
+import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <App />
+    <React.StrictMode>
+      <FirebaseContext.Provider value={{ firebase }}>
+        <GlobalStyles />
+        <App />
+      </FirebaseContext.Provider>
+    </React.StrictMode>
   </>,
   document.getElementById('root')
 );
