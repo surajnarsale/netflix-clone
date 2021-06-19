3b87d9cfe9da35d0b5fe72f361122db715a9ccd1
Suraj Narsale <surajsnarsale@gmail.com>
2021-06-20 01:04:36 +0530

    fix: env variable typo

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,7 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: process.env.REACT_API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
