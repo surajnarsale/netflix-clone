e8c0cdaa1f9a28761948e49b89be7b085fc5ce80
Suraj Narsale <surajsnarsale@gmail.com>
2021-06-24 08:20:00 +0530

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, Browse, Signin, Signup } from './pages/index';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect } from './helpers/routes';
 
 export default function App() {
+  const user = {};
   return (
     <>
       <Router>
-        <Route exact path="/browse">
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_IN}
+          exact
+        />
+        <Signin />
+
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+          exact
+        />
+        <Signup />
+
+        <Route exact path={ROUTES.BROWSE}>
           <Browse />
-        </Route>
-        <Route exact path="/signin">
-          <Signin />
-        </Route>
-        <Route exact path="/signup">
-          <Signup />
         </Route>
         <Route exact path={ROUTES.HOME}>
           <Home />
